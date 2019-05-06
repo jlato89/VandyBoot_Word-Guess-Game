05217cad6e3640b9f66ae7017344d10c5e2d2347
Josh Latour <jlatour88@gmail.com>
2019-05-06 14:31:51 -0500

    trying out diff functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,10 @@
 var words = ['computer', 'remote', 'monitor', 'keyboard', 'desktop', 'laptop', 'headset', 'mouse', 'joystick', 'gamepad'];
 var number = Math.floor(Math.random() * words.length);
 var currentWord = words[number];
+var currentWordSplit = currentWord.split("");
+var currentDashesSplit = 
+var currentDashesSplit = currentWord.length;
+
 
 var guessesRemaining = 15;
 var lettersGuessed = "NOT SET YET";
@@ -33,23 +37,30 @@ document.getElementById("letters-guessed").innerHTML = lettersGuessed;
 document.onkeyup = function (event) {
     
     var userKey = event.key.toLowerCase();
+    var search = /userKey/g;
 
-    if (userKey == currentWord.search) {
+    if (currentWord.search(userKey) !== -1) {
         alert("You Matched!")
     }
 
-// repeat and includes for above
+    // var str = "Visit W3Schools!"; 
+    // var n = str.search("w");
+    // document.getElementById("demo").innerHTML = n;
 
+// repeat and includes for above
     console.log(userKey);
+    console.log('currentWord.search(userKey): ', currentWord.search(userKey));
+
 };
 
 
     // CONSOLE LOG
     // ==========================================================================
 
+console.log("-----------");
 // console.log(guessesRemaining);
 // console.log(words);
 // console.log(number);
 console.log(currentWord);
-// console.log(wordDashes);
-// console.log("-".repeat(wordDashes));
+console.log(currentWordSplit);
+console.log(currentDashesSplit);
