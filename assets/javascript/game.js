09f61069e0f7a334dfdfab5499b2ffd4261b0c5c
Josh Latour <jlatour88@gmail.com>
2019-05-05 19:04:19 -0500

    started adding var and functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,55 @@
-// VARIABLES
-// ==========================================================================
+    // VARIABLES
+    // ==========================================================================
 
-// var userText = document.getElementById('user-text');
-var gameWords = ['computer', 'remote', 'monitor', 'keyboard', 'desktop', 'laptop', 'headset', 'mouse', 'joystick', 'gamepad'];
-var number = Math.floor(Math.random() * gameWords.length);
-// var currentWord = document.getElementById('current-word');
-var wordLetters = [];
+var words = ['computer', 'remote', 'monitor', 'keyboard', 'desktop', 'laptop', 'headset', 'mouse', 'joystick', 'gamepad'];
+var number = Math.floor(Math.random() * words.length);
+var currentWord = words[number];
 
-var guessesRemaining = 0;
-
-
-// FUNCTIONS
-// ==========================================================================
-
-function guessesRemaining() { 
-    var guessesRemaining = 9;
-    document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
-  }
+var guessesRemaining = 15;
+var lettersGuessed = "NOT SET YET";
 
 
-// MAIN PROCESS
-// ==========================================================================
+
+document.getElementById("current-word").innerHTML = "- ".repeat(currentWord.length);
+document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+document.getElementById("letters-guessed").innerHTML = lettersGuessed;
+
+
+    // FUNCTIONS
+    // ==========================================================================
+
+    
+
+// function guessesRemaining() { 
+//     var guessesRemaining = 9;
+//     document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+//   }
+
+
+    // MAIN PROCESS
+    // ==========================================================================
 
 // Captures keyboard input.
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     
-    var letter = event.key.toLowerCase();
+    var userKey = event.key.toLowerCase();
 
-    if (letter === wordLetters) {
-        // fill in letter for current word, add letter to guessed letters
-    }else {
-        // add letter to guessed letters, subtract 1 from guesses remaining
+    if (userKey == currentWord.search) {
+        alert("You Matched!")
     }
+
+// repeat and includes for above
+
+    console.log(userKey);
 };
 
 
-// CONSOLE LOG
-// ==========================================================================
+    // CONSOLE LOG
+    // ==========================================================================
 
-console.log(guessesRemaining);
-// console.log(currentWord);
+// console.log(guessesRemaining);
+// console.log(words);
+// console.log(number);
+console.log(currentWord);
+// console.log(wordDashes);
+// console.log("-".repeat(wordDashes));
