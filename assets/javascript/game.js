91588ad2442baa00509a815b6b25296635bad7ba
Josh Latour <jlatour88@gmail.com>
2019-05-07 11:47:58 -0500

    created for loop for userkey match

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,10 @@
 var words = ['computer', 'remote', 'monitor', 'keyboard', 'desktop', 'laptop', 'headset', 'mouse', 'joystick', 'gamepad'];
 var number = Math.floor(Math.random() * words.length);
 var currentWord = words[number];
-var currentWordSplit = currentWord.split("");
-var currentDashesSplit = 
-var currentDashesSplit = currentWord.length;
 
+// Arrays
+var currentWordArray = currentWord.split("");
+var currentDashArray = [];
 
 var guessesRemaining = 15;
 var lettersGuessed = "NOT SET YET";
@@ -22,12 +22,6 @@ document.getElementById("letters-guessed").innerHTML = lettersGuessed;
     // FUNCTIONS
     // ==========================================================================
 
-    
-
-// function guessesRemaining() { 
-//     var guessesRemaining = 9;
-//     document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
-//   }
 
 
     // MAIN PROCESS
@@ -35,32 +29,36 @@ document.getElementById("letters-guessed").innerHTML = lettersGuessed;
 
 // Captures keyboard input.
 document.onkeyup = function (event) {
-    
+
     var userKey = event.key.toLowerCase();
-    var search = /userKey/g;
 
-    if (currentWord.search(userKey) !== -1) {
-        alert("You Matched!")
+    for (i = 0; i < currentWord.length; i++) {
+
+        if (currentWordArray[i] === userKey) {
+            // alert("You Matched!");
+            console.log("userKey " + userKey + " Matched " + "current word index of " + currentWordArray[i])
+            // currentDashArray[i] = userKey;
+        };
+
+
+// for statement console logs    
+    // console.log(userKey);
+    // console.log(currentWordArray[i]);
+
     }
-
-    // var str = "Visit W3Schools!"; 
-    // var n = str.search("w");
-    // document.getElementById("demo").innerHTML = n;
-
-// repeat and includes for above
-    console.log(userKey);
-    console.log('currentWord.search(userKey): ', currentWord.search(userKey));
-
 };
+
+
 
 
     // CONSOLE LOG
     // ==========================================================================
 
-console.log("-----------");
+// console.log("-----------");
 // console.log(guessesRemaining);
 // console.log(words);
 // console.log(number);
 console.log(currentWord);
-console.log(currentWordSplit);
-console.log(currentDashesSplit);
+console.log(currentWordArray);
+// console.log(currentWordLength);
+// console.log(currentDashesSplit);
